b8f81f47fa8b9947414613c98949199277b2bbd7
rohit shyam mane <rohitmane8356@gmail.com>
2024-01-08 01:41:58 +0530

    intial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './Components/Header'
+import Main from './Components/Main'
+
+
+function App() {
+
+  return (
+   <div className='px-2 xl:px-72 lg:px-48 sm:px-8 md:px-24 text-gray-200'>
+    <Header/>
+    <Main/>
+   </div>
+  )
+}
+
+export default App
