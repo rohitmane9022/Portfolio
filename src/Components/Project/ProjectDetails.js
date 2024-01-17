87d9186ab55bb2a7d7bc47449720cd9023865fd2
Rohit Mane <74919679+rohitmane9022@users.noreply.github.com>
2024-01-17 12:59:29 +0530

    Update ProjectDetails.js

--- src/Components/Project/ProjectDetails.js
+++ src/Components/Project/ProjectDetails.js
@@ -2,7 +2,7 @@
 
 export const ProjectDeatils=[
   {
-    liveLink: 'https://air-bnb-clone-mu-one.vercel.app/',
+    liveLink: 'https://air-bnb-clone-ivory.vercel.app/',
     sourceLink: 'https://github.com/rohitmane9022/AirBnb_Clone',
     id: 'AirBnb_Clone',
     title: 'AirBnb_Clone',
