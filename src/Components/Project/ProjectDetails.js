4f802d65c4b33617b5cd9a4443e713ed73adff3b
Rohit Mane <74919679+rohitmane9022@users.noreply.github.com>
2024-05-23 22:18:03 +0530

    Update ProjectDetails.js

--- src/Components/Project/ProjectDetails.js
+++ src/Components/Project/ProjectDetails.js
@@ -1,14 +1,14 @@
 
 
 export const ProjectDeatils=[
-  {
-    liveLink: 'https://airbnb-clone-made.netlify.app/',
-    sourceLink: 'https://github.com/rohitmane9022/AirBnb_Clone',
-    id: 'AirBnb_Clone',
-    title: 'AirBnb_Clone',
-    img: 'https://res.cloudinary.com/dj1awovlx/image/upload/v1704813313/Screenshot_2024-01-09_204457_sno4qm.png',
-    techStack: 'React, Context API, Tailwind CSS and Auth using JWT',
-  },
+  // {
+  //   liveLink: 'https://airbnb-clone-made.netlify.app/',
+  //   sourceLink: 'https://github.com/rohitmane9022/AirBnb_Clone',
+  //   id: 'AirBnb_Clone',
+  //   title: 'AirBnb_Clone',
+  //   img: 'https://res.cloudinary.com/dj1awovlx/image/upload/v1704813313/Screenshot_2024-01-09_204457_sno4qm.png',
+  //   techStack: 'React, Context API, Tailwind CSS and Auth using JWT',
+  // },
   {
     liveLink: 'https://ecommerce-ob2s.vercel.app/',
     sourceLink: 'https://github.com/rohitmane9022/ecommerce',
