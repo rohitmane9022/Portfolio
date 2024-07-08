950f60b24647bc31e547cdbdaff0774b04872bb3
Rohit Mane <74919679+rohitmane9022@users.noreply.github.com>
2024-07-08 13:06:32 +0530

    Update ProjectDetails.js

--- src/Components/Project/ProjectDetails.js
+++ src/Components/Project/ProjectDetails.js
@@ -1,14 +1,6 @@
 
 
 export const ProjectDeatils=[
-  // {
-  //   liveLink: 'https://airbnb-clone-made.netlify.app/',
-  //   sourceLink: 'https://github.com/rohitmane9022/AirBnb_Clone',
-  //   id: 'AirBnb_Clone',
-  //   title: 'AirBnb_Clone',
-  //   img: 'https://res.cloudinary.com/dj1awovlx/image/upload/v1704813313/Screenshot_2024-01-09_204457_sno4qm.png',
-  //   techStack: 'React, Context API, Tailwind CSS and Auth using JWT',
-  // },
   {
     liveLink: 'https://ecommerce-ob2s.vercel.app/',
     sourceLink: 'https://github.com/rohitmane9022/ecommerce',
@@ -16,6 +8,14 @@ export const ProjectDeatils=[
     title: 'Nike-Ecommerce',
     img: 'https://res.cloudinary.com/dj1awovlx/image/upload/v1704804755/Screenshot_2024-01-09_182204_bqyv7e.png',
     techStack: 'React, Context API, Vanilla CSS and Auth using JWT',
+  },
+  {
+    liveLink: 'https://air-bnb-clone-delta.vercel.app/',
+    sourceLink: 'https://github.com/rohitmane9022/AirBnb-Clone',
+    id: 'AirBnb_Clone',
+    title: 'AirBnb_Clone',
+    img: 'https://res.cloudinary.com/dj1awovlx/image/upload/v1704813313/Screenshot_2024-01-09_204457_sno4qm.png',
+    techStack: 'React, Context API, Tailwind CSS and Auth using JWT',
   },
   {
     liveLink: 'https://social-media-rouge-five.vercel.app/',
