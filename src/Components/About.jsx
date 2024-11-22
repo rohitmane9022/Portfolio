03d2e68b8a4b476524156935e52fe3baba313836
Rohit Mane <74919679+rohitmane9022@users.noreply.github.com>
2024-11-22 22:19:22 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
       <section className="my-8 flex flex-row items-center justify-between px-3">
         <div>
           <h1 className="text-4xl font-bold">Rohit Mane.</h1>
-          <p className="mt-2 text-lg">Full Stack Developer, Blogger</p>
+          <p className="mt-2 text-lg">Frontend Developer, Blogger</p>
           <p className="mt-4 text-gray-400">Building Web apps, proficient in Frontend.</p>
           <Link to="https://rohitmane9022.hashnode.dev/" target="_blank" className="group mt-4 flex items-center gap-1 flex-row text-xl text-gray-400 transition-all duration-150 hover:text-white">Read my Blogs
           <AiOutlineArrowRight className="mt-1 transition-all duration-100 group-hover:ml-1 "/></Link>
