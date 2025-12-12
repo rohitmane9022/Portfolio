437e2c6ad444dc9f56d91e59ba02999145f0af77
rohitmane9022 <rohitmane8356@gmail.com>
2025-12-12 22:21:51 +0530

    Add SiteMap

---
+++ app/sitemap.js
@@ -0,0 +1,35 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://www.rohitmane.space/',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 1.0,
+      },
+      {
+        url: 'https://www.rohitmane.space/about',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://www.rohitmane.space/experience',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://www.rohitmane.space/projects',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://www.rohitmane.space/contact',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.7,
+      },
+    ];
+  }
+  
